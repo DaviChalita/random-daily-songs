52f12b09393f8dcf8f26cf0dbe15b2fa448f1c21
DaviChalita <chalitameneguelli14@gmail.com>
2023-03-23 11:45:12 -0300

    setting object genre + preview url

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
+import random
+
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+
+
+class Track:
+    def __init__(self, genre_track, song_track):
+        self.genre_track = genre_track
+        self.song_track = song_track
+
 
 sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
 with open('genres.txt') as file:
     genres_list = [line.rstrip() for line in file]
 
-for genre in genres_list:
-    sp.search(genre)
+tracks_list = []
 
+for genre in genres_list:
+    total = sp.search(genre, limit=1)['tracks']['total']
+    offset = random.randint(0, total)
+    items = sp.search(genre, limit=1, offset=offset)['tracks']['items']
+    if len(items) > 0:
+        song = items[0]['preview_url']
+        tracks_list.append(Track(genre, song))
+
+a = 0
