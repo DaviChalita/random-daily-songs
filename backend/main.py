13ce064d207961b2d6515e150b3f785bc8cf1568
Davi Chalita <chalitameneguelli14@gmail.com>
2024-04-15 22:29:34 -0300

    add cron, fix track generation bug

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,17 @@
 import json
+import logging
 import random
 
 import spotipy
 from flask import Flask
+from flask_apscheduler import APScheduler
 from spotipy.oauth2 import SpotifyClientCredentials
 
 app = Flask(__name__)
+scheduler = APScheduler()
 
 
-@app.route('/random-daily-songs/generate', methods=['GET'])
+@scheduler.task('cron', id='generate', day_of_week='*', hour=0, minute=0)
 def generate():
     class Track:
         def __init__(self, genre_track, song_track):
@@ -21,14 +24,18 @@ def generate():
         genres_list = [line.rstrip() for line in file]
 
     tracks_list = []
+    genres_list = random.sample(genres_list, 10)
+    app.logger.info('Genres: \n' + ', '.join(genres_list))
 
-    for genre in genres_list[:10]:
-        app.logger.info('Genre: ', genre)
+    for genre in random.sample(genres_list, 10):
         total = sp.search(genre, limit=1)['tracks']['total']
         offset = random.randint(0, total)
-        items = sp.search(genre, limit=1, offset=offset)['tracks']['items']
+        items = sp.search('genre:' + genre, limit=10, offset=offset)['tracks']['items']
         if len(items) > 0:
-            song = items[0]['preview_url']
+            i = 0
+            while items[i]['preview_url'] is None and i < len(items):
+                i += 1
+            song = items[i]['preview_url']
             tracks_list.append(Track(genre, song))
     f = open("generated.json", "w")
     f.write(json.dumps([ob.__dict__ for ob in tracks_list], indent=len(tracks_list)))
@@ -43,4 +50,7 @@ def get():
 
 
 if __name__ == '__main__':
+    scheduler.init_app(app)
+    scheduler.start()
+    app.logger.setLevel(logging.INFO)
     app.run(host='0.0.0.0', port=105)
